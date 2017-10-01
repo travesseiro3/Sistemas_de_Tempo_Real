90df37fb69a634fff47678e604111a4bbd934a83
alex7alves <alex7alves10@hotmail.com>
2017-10-01 15:38:58 -0300

    Indentando o  código

--- Trabalho 2.2/trab22.cpp
+++ Trabalho 2.2/trab22.cpp
@@ -17,102 +17,99 @@ using namespace BlackLib;
 
 void carga(int k)
 {
-  float f = 0.999999;
-  for(int i=0; i<k; i++)
-    {
-      f = f*f*f*f*f;
-      f = 1.56;
-       for(int j=0; j<k; j++)
-      {
-	        f = sin(f)*sin(f)*f*f*f;
+	float f = 0.999999;
+	for(int i=0; i<k; i++)
+		{
+			f = f*f*f*f*f;
+			f = 1.56;
+			 for(int j=0; j<k; j++)
+			{
+					f = sin(f)*sin(f)*f*f*f;
 
-      }
-    }
+			}
+		}
 }
 
 
 int main()
 {
-    BlackGPIO led2(GPIO_67,output);
-    BlackGPIO led1(GPIO_60,output);  
-    ADC pot1(AIN0);
-    ADC pot2(AIN1);       
-    int filho1,filho2 ;
-    int cont1=0,cont2=0;
-    filho1 =fork();
-    if(filho1==-1){
-        std::cout << " Nao criou filhho 1 " << endl;
-    }else if (filho1 ==0){
-        cont1=0;
-        while(1){
-          carga(1200);
-          if(cont1%2==0){
-                led1.setValue(high);
+		BlackGPIO led2(GPIO_67,output);
+		BlackGPIO led1(GPIO_60,output);  
+		ADC pot1(AIN0);
+		ADC pot2(AIN1);       
+		int filho1,filho2 ;
+		int cont1=0,cont2=0;
+		filho1 =fork();
+		if(filho1==-1){
+				std::cout << " Nao criou filhho 1 " << endl;
+		}else if (filho1 ==0){
+				cont1=0;
+				while(1){
+					carga(1200);
+					if(cont1%2==0){
+								led1.setValue(high);
 
-           }else {
-                led1.setValue(low);
-           } 
-        
-           cont1++; 
-        }
-    }else {
+					 }else {
+								led1.setValue(low);
+					 } 
+				
+					 cont1++; 
+				}
+		}else {
 
-        filho2 =fork();
-        if(filho2==-1){
-            std::cout << " Nao criou filhho 1 " << endl;
-        }else if (filho2 ==0){
-            cont2=0;
-            while(1){
-              carga(1200);
-              if(cont2%2==0){
-                    led2.setValue(high);
+				filho2 =fork();
+				if(filho2==-1){
+						std::cout << " Nao criou filhho 1 " << endl;
+				}else if (filho2 ==0){
+						cont2=0;
+						while(1){
+							carga(1200);
+							if(cont2%2==0){
+										led2.setValue(high);
 
-               }else {
-                    led2.setValue(low);
-               } 
-            
-               cont2++; 
-            }       
-        }else {
-        	  float  val = pot1.getFloatValue();
-	          float  val2 = pot2.getFloatValue();
-        	  if(val==0 && val2==0 ){
-        		  led1.setValue(low);
-        	    led2.setValue(low);
-        	  }else {
-          		while(1){       
-  	                val = pot1.getFloatValue();
-  	                val2  = pot2.getFloatValue();
-                    setpriority(PRIO_PROCESS,getpid(),0);
-                    usleep(500000);
-                    if(val >1){
-                        setpriority(PRIO_PROCESS, filho1, 5);
-  		                  std::cout<<" Potenciometro 1 "<<pot1.getFloatValue() << endl;
-                    }else {
-                        setpriority(PRIO_PROCESS, filho1, 19);
-  		                  std::cout<<" Potenciometro 1 "<<pot1.getFloatValue() << endl;
+							 }else {
+										led2.setValue(low);
+							 } 
+						
+							 cont2++; 
+						}       
+				}else {
+						
+						
+							led1.setValue(low);
+							led2.setValue(low);
+						
+							while(1){       
+										float val = pot1.getFloatValue();
+										float val2  = pot2.getFloatValue();
+										setpriority(PRIO_PROCESS,getpid(),0);
+										usleep(500000);
+										if(val >1){
+												setpriority(PRIO_PROCESS, filho1, 5);
+												std::cout<<" Potenciometro 1 "<<pot1.getFloatValue() << endl;
+										}else {
+												setpriority(PRIO_PROCESS, filho1, 19);
+												std::cout<<" Potenciometro 1 "<<pot1.getFloatValue() << endl;
 
-                    }
+										}
 
-                    if(val2 >1){
-                        setpriority(PRIO_PROCESS, filho2, 5);
-                       	std::cout<<" Potenciometro 2 "<<pot2.getFloatValue() << endl;
-  		              }else {
-    	                  setpriority(PRIO_PROCESS, filho2, 19);
-    			              std::cout<<" Potenciometro 2 "<<pot2.getFloatValue() << endl;
-    	              }
-    	              if(val==0 && val2==0){
-      		        		  led1.setValue(low);
-      		        	    led2.setValue(low);
-                        std::cout<< " Programa finalizado , ate a proxia :) " << endl;
-      		        	    break;
-      		        	}
-    	          }
-	            
-        	}
-	         
-        }
-    }  
-     return 0;
+										if(val2 >1){
+												setpriority(PRIO_PROCESS, filho2, 5);
+												std::cout<<" Potenciometro 2 "<<pot2.getFloatValue() << endl;
+										}else {
+												setpriority(PRIO_PROCESS, filho2, 19);
+												std::cout<<" Potenciometro 2 "<<pot2.getFloatValue() << endl;
+										}
+										if(val==0 && val2==0){
+												led1.setValue(low);
+												led2.setValue(low);
+												std::cout<< " Programa finalizado , ate a proxia :) " << endl;
+												break;
+										}
+								}
+					 
+				}
+		}  
+		 return 0;
 }
 
