981dfadb8eced625ac458ad4492080cd313f56cd
alex7alves <alex7alves10@hotmail.com>
2017-11-07 17:42:10 -0300

    Corrigindo trabalho

--- Trabalho 2.4/Trilhos_treen_beagle.cpp
+++ Trabalho 2.4/Trilhos_treen_beagle.cpp
@@ -25,7 +25,7 @@
 using namespace BlackLib;
 
 void *thread_function(void *arg); 
-int t1=1,t2=5;
+int t1=1,t2=7;
 pthread_mutex_t work_mutex; 
 
 //Portas digitais utilizadas no display de 7 segmentos (anodo comum) sem o Dot Point
@@ -45,29 +45,29 @@ void Ligar(int n){
     switch(n){
 
         case 1:
-            a.setValue(high); 
-            sleep(tempo);
+            a.setValue(low); 
+          //  sleep(tempo);
             break;
         case 2:
-            b.setValue(high);
-            sleep(tempo);
+            b.setValue(low);
+          //  sleep(tempo);
            break;
         
         case 4:     
-            f.setValue(high);
-            sleep(tempo);
+            f.setValue(low);
+          //  sleep(tempo);
             break;
         case 5:
-            c.setValue(high);
-            sleep(tempo2);
+            c.setValue(low);
+           // sleep(tempo2);
             break;
         case 6:
-            d.setValue(high);
-            sleep(tempo2);
+            d.setValue(low);
+            
             break;
         case 7: 
-            e.setValue(high);
-            sleep(tempo2);
+            e.setValue(low);
+           
             break;
         
     }
@@ -77,38 +77,38 @@ void Desligar(int n){
     switch(n){
 
         case 0:
-            a.setValue(low); 
-            b.setValue(low); 
-            c.setValue(low); 
-            d.setValue(low); 
-            e.setValue(low); 
-            f.setValue(low); 
-            g.setValue(low); 
+            a.setValue(high); 
+            b.setValue(high); 
+            c.setValue(high); 
+            d.setValue(high); 
+            e.setValue(high); 
+            f.setValue(high); 
+            g.setValue(high); 
             break;
         
         case 1:
-            a.setValue(low); 
+            a.setValue(high); 
             break;
         case 2:
-            b.setValue(low);
+            b.setValue(high);
            break;
          case 3:     
-            g.setValue(low);
+            g.setValue(high);
             break;
         case 4:     
-            f.setValue(low);
+            f.setValue(high);
             break;
         case 5:
-            c.setValue(low);
+            c.setValue(high);
             break;
         case 6:
-            d.setValue(low);
+            d.setValue(high);
             break;
         case 7: 
-            e.setValue(low);
+            e.setValue(high);
             break;
          case 8:     
-            g.setValue(low);
+            g.setValue(high);
             break;
         
     }
@@ -117,11 +117,7 @@ void Desligar(int n){
 int main(){
     //Porta digital utilizada para o PushButton
     BlackGPIO entrada(GPIO_65, input);
-    int val;
-    float pot1,pot2;
-    //para alimentar os potenciômetros, foi utilizado o VCC_ADC e o GNDA_ADC
-    ADC potEsquerdo(AIN3);
-    ADC potDireito(AIN5);  
+     
     int res,res2,v1=1,v2=2;
     pthread_t thread_1,thread_2;
     void *thread_result;
@@ -132,50 +128,39 @@ int main(){
         perror("Iniciação do Mutex falhou");
         exit(EXIT_FAILURE);
     }
-    
-    while(1) {
-        // lendo o botao
-        val = entrada.getNumericValue();
-        if(!val){
-            Desligar(0);
-            std::cout<<" Resetando "<< endl;
-            sleep(1);
-        }else {
-            //lendo os potenciomentros
-            pot1 = potEsquerdo.getFloatValue();
-            pot2 = potDireito.getFloatValue();
-            if(pot1==0 && pot2==0){
-                break;
-            }
-            if(pot1>0){
-                tempo = 1/pot1;
-            }
-            if(pot2>0){
-                tempo2 = 1/pot2;
-            }
-            
-            // Criar thread
+    // Criar thread
             res = pthread_create(&thread_1, NULL, thread_function,&v1);
             res2 = pthread_create(&thread_2, NULL, thread_function,&v2);
             if (res != 0 || res2 != 0) {
                 perror("Criação da Thread falhou");
                 exit(EXIT_FAILURE);
             }
+   // while(1) {
+        // lendo o botao
+      /*  val = entrada.getNumericValue();
+        if(!val){
+            Desligar(0);
+            std::cout<<" Resetando "<< endl;
+            sleep(1);
+        }else {
            
-            if(t1>4){
-                t1=1;
-            }
-            if(t2>8){
-                t2=5;
-            }
+            
+           
+            
             res = pthread_join(thread_1, &thread_result);
             res2 = pthread_join(thread_2, &thread_result);
             if (res != 0 || res2 != 0) {
                 perror("Join falhou");
                 exit(EXIT_FAILURE);
+            }**/
+       // }
+    res = pthread_join(thread_1, &thread_result);
+            res2 = pthread_join(thread_2, &thread_result);
+            if (res != 0 || res2 != 0) {
+                perror("Join falhou");
+                exit(EXIT_FAILURE);
             }
-        }
-    }
+    
     
     pthread_mutex_destroy(&work_mutex);  // destruição do multex
     exit(EXIT_SUCCESS);
@@ -186,16 +171,45 @@ int main(){
 
 
 void *thread_function(void *arg) {
+    int val;
+    float pot1,pot2;
+    //para alimentar os potenciômetros, foi utilizado o VCC_ADC e o GNDA_ADC
+    ADC potEsquerdo(AIN3);
+    ADC potDireito(AIN5); 
     int *L= (int*)arg;
+    while(1){
+     //lendo os potenciomentros
+            pot1 = potEsquerdo.getFloatValue();
+            pot2 = potDireito.getFloatValue();
+            std::cout<<"Valor do ADC1  "<< pot1 << endl;
+            std::cout<<"Valor do ADC2  "<< pot2 << endl;
+           /* if(pot1==0 && pot2==0){
+               std::cout<<"acabou "< << endl;
+                break;
+            }*/
+            if(pot1>0){
+                tempo = 1/pot1;
+            }
+            if(pot2>0){
+                tempo2 = 1/pot2;
+            }
+            if(t1>4){
+                t1=1;
+            }
+            if(t2>8){
+                t2=5;
+            }
     if(*L==1 && t1!=3){
         std::cout<<"Thread 1 na linha  "<< t1 << endl;
         Ligar(t1);
+        sleep(tempo);
         t1++;
         Desligar(t1);
     }
     if(*L==2 &&  t2!=8){
         std::cout<<"Thread 2 na linha  "<< t2 << endl;
         Ligar(t2);
+         sleep(tempo2);
         t2++;
         Desligar(t2);
     }
@@ -204,18 +218,19 @@ void *thread_function(void *arg) {
         // threns 1 ou 2 na linha 3 
         if(*L==1 && t1==3){
             std::cout<<"Thread 1 na linha  especial  "<< endl;
-            g.setValue(high);          
+            g.setValue(low);          
             t1++; 
             sleep(tempo);
             Desligar(t1);
         }
         if(*L==2 && t2==8){
             std::cout<<"Thread 2 na linha  especial 3 "<< endl;
-            g.setValue(high);
+            g.setValue(low);
             t2++; 
             sleep(tempo2);
             Desligar(t2);
         }
     pthread_mutex_unlock(&work_mutex);
+    }
     pthread_exit(0);
 }
